eda70201766f6ee2e3bcb662cb9a11beab18b468
Artur Mylnikov <kuraidatenshidesu@gmail.com>
2023-02-10 15:13:49 +0300

    Commit after lessons

--- main.c
+++ main.c
@@ -2,28 +2,97 @@
 #include "raylib.h"
 
 #include "map.h"
-#include "enemy.h"
 
 int main(void)
 {
     const int screenWidth = 800;
-    const int screenHeight = 640;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+    SetWindowSize(GetMonitorWidth(0), GetMonitorHeight(0));
+    ToggleFullscreen();
+
+    int width = GetScreenWidth();
+    int height = GetScreenHeight();
+
+    Texture2D PlayerTextureRight = LoadTexture("img/player_right.png");
+    Texture2D PlayerTextureLeft = LoadTexture("img/player_left.png");
+    Texture2D current = PlayerTextureRight;
+
+    Vector2 playerPosition = (Vector2){ (float)500, (float)500 };
+
+    bool exitWindowRequested = false;
+    bool exitWindow = false;
+
+    // Color myColor = {255, 0, 102, 1};
 
     SetTargetFPS(60);
 
-    Map_Prepare(10, 10);
+    Map_Prepare(50, 50);
     Map_Fill();
-    Map_Draw();
 
-    while (!WindowShouldClose())
+    float speed = 10.0f;
+
+    while (!exitWindow)
     {
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) 
+        {
+            if(playerPosition.x + speed < width - 100)
+            {
+                playerPosition.x += speed;
+                current = PlayerTextureRight;
+            }
+        }
+
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+        {
+            if(playerPosition.x - speed > 0)
+            {
+                playerPosition.x -= speed;
+                current = PlayerTextureLeft;
+            }
+        }
+
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
+        {
+            if(playerPosition.y - speed > 0){
+                playerPosition.y -= speed;
+            }
+        }
+
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+        {
+            if(playerPosition.y + speed < height - 100)
+            {
+                playerPosition.y += speed;
+            }
+        }
+
+        if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
+        {
+            exitWindowRequested = true;
+        }
+
+        if (exitWindowRequested)
+        {
+            if (IsKeyPressed(KEY_Y)) exitWindow = true;
+            else if (IsKeyPressed(KEY_N)) exitWindowRequested = false;
+        }
+
         BeginDrawing();
+            ClearBackground(BLACK);
 
-        ClearBackground(RAYWHITE);
+            Map_Draw(50);
 
-        DrawText("Game Would Be Here", screenWidth / 2, screenHeight / 2, 20, BLACK);
+            DrawTextureEx(current, playerPosition, 0, 0.125, WHITE);
+
+            DrawFPS(50, 50);
+            
+            if (exitWindowRequested)
+            {
+                DrawRectangle(0, 0, width, height + 10, BLACK);
+                DrawText("Quit the game? [Y/N]", (float) width / 3, (float) height / 3, 30, WHITE);
+            }
 
         EndDrawing();
     }
--- src/map.c
+++ src/map.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "raylib.h"
 
 #include "pair.h"
 
 struct
 {
     Pair pair;
-    char *matrix;
+    int *matrix;
 } Map;
+
+Texture2D grass;
+Texture2D stone;
 
 void Map_Prepare(int x, int y)
 {
+    grass = LoadTexture("img/grass.png");
+    stone = LoadTexture("img/stone.png");
     Map.pair.x = x;
     Map.pair.y = y;
-    Map.matrix = (char *)malloc(sizeof(char) * x * y);
+    Map.matrix = (int *)malloc(sizeof(int) * x * y);
 }
 
 void Map_Fill()
 {
+    srand(time(NULL));
+    
     for (int x = Map.pair.x - 1; x >= 0; x--)
     {
         for (int y = Map.pair.y - 1; y >= 0; y--)
         {
-            Map.matrix[x * Map.pair.x + y] = 'O';
+            int r = rand() % 2 + 1;
+            Map.matrix[x * Map.pair.x + y] = r;
         }
     }
 }
 
-void Map_Draw()
+Texture2D current;
+void Map_Draw(int tileSize)
 {
     for (int x = Map.pair.x - 1; x >= 0; x--)
     {
         for (int y = Map.pair.y - 1; y >= 0; y--)
         {
-            printf("%c ", Map.matrix[x * Map.pair.x + y]);
+            if (Map.matrix[x * Map.pair.x + y] == 1)
+            {
+                current = stone;
+            }
+
+            if (Map.matrix[x * Map.pair.x + y] == 2)
+            {
+                current = grass;
+            }
+
+            DrawTexture(current, x * tileSize, y * tileSize, WHITE);
         }
-        printf("\n");
     }
 }
