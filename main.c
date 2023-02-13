a5db5a32b1fe618b29b30257c7db48770bcc03e3
Artur Mylnikov <kuraidatenshidesu@gmail.com>
2023-02-13 21:45:33 +0300

    Added Player

--- main.c
+++ main.c
@@ -1,17 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "raylib.h"
+
+struct
+{
+	Vector2 coordinates;
+	float speed;
+} Player;
+
+void Player_Init(Vector2 coordinates, float speed)
+{
+	Player.coordinates.x = coordinates.x;
+	Player.coordinates.y = coordinates.y;
+	Player.speed = speed;
+}
 
 int main(void)
 {
 	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenHeight = 600;
 
 	InitWindow(screenWidth, screenHeight, "Game");
 	SetTargetFPS(60);
 
+	Player_Init((Vector2){100, 100}, 5.0f);
+
 	while (!WindowShouldClose())
 	{
-		BeginDrawing();
+		if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+		{
+			Player.coordinates.x -= Player.speed;
+		}
 
+		if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+		{
+			Player.coordinates.x += Player.speed;
+		}
+
+		if (IsKeyDown(KEY_A) || IsKeyDown(KEY_RIGHT))
+		{
+			Player.coordinates.y -= Player.speed;
+		}
+
+		if (IsKeyDown(KEY_D) || IsKeyDown(KEY_LEFT))
+		{
+			Player.coordinates.y += Player.speed;
+		}
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawRectangle(Player.coordinates.y, Player.coordinates.x, 100, 100, WHITE);
 		EndDrawing();
 	}
 
